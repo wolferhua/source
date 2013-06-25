d7f3d6488c8786364611e83a8ec80222ac4789d1
wuhua <358273385@qq.com>
2013-06-25 14:35:14 +0800

    函数库
    增加 timerCache函数

---
+++ functions.js
@@ -0,0 +1,27 @@
+/*!
+ * Functions 
+ * http://jquery.com/
+ *
+ * Copyright 2013, hua wu 
+ * 
+ *
+ * Date: 2013-6-25
+ */
+var func_timerCaches = {};
+//**
+//* 优化的缓存定时器
+//* fun mixed 要执行的函数
+//* time unint 间隔时间
+function timerCache(fun,time){
+    if(fun== null) return false;
+    if(time==null){
+        if( func_timerCaches[fun] ){
+            clearTimeout(func_timerCaches[fun]);
+        }
+        func_timerCaches[fun] = false;
+    }else{
+        timerCache(fun);
+        func_timerCaches[fun] = setTimeout(fun,time);
+        return func_timerCaches[fun];
+    }
+}
